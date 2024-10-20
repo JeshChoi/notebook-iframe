c88f3570f711a5d31dac7b0240944329884039c2
Joshua <choiji2@uci.edu>
2024-10-19 23:09:50 -0700

    notebook to workflow

--- jupyterlab-iframe/custom.js
+++ jupyterlab-iframe/custom.js
@@ -12,32 +12,34 @@ require(["base/js/events"], function (events) {
       const index = $(".cell").index(cell);
       const cellContent = cell.find(".input_area").text();
 
-      console.log(`Cell ${index + 1} clicked:`, cellContent);
+      // Get the UUID from the cell's metadata, or use "N/A" if it doesn't exist
+      const cellUUID = Jupyter.notebook.get_cell(index).metadata.uuid || 'N/A';
+
+      console.log(`Cell ${index + 1} clicked:`, cellContent, `UUID: ${cellUUID}`);
 
       // Send a message to the parent window (React app)
       // Replace 'http://localhost:3000' with your React app's actual origin
       window.parent.postMessage(
-        { action: "cellClicked", cellIndex: index, cellContent: cellContent },
-        "http://localhost:3000"
+        { action: "cellClicked", cellIndex: index, cellContent: cellContent, cellUUID: cellUUID },
+        "http://localhost:4200"
       );
     });
   });
 });
 
 // Listen for messages from the parent window
-window.addEventListener("message", function(event) {
-    // Verify the message origin
-    if (event.origin !== 'http://localhost:3000') return;
-  
-    console.log("Received message from parent:", event.data);
-  
-    if (event.data.action === "triggerCellClick") {
-      // Simulate a cell click inside the Jupyter notebook
-      let cellIndex = event.data.cellIndex;
-      let cell = document.querySelectorAll(".cell")[cellIndex];
-      if (cell) {
-        cell.click();
-      }
+window.addEventListener("message", function (event) {
+  // Verify the message origin
+  if (event.origin !== 'http://localhost:4200') return;
+
+  console.log("Received message from parent:", event.data);
+
+  if (event.data.action === "triggerCellClick") {
+    // Simulate a cell click inside the Jupyter notebook
+    let cellIndex = event.data.cellIndex;
+    let cell = document.querySelectorAll(".cell")[cellIndex];
+    if (cell) {
+      cell.click();
     }
-  }, false);
-  
+  }
+}, false);
